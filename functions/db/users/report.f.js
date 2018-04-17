33173bc7e70949c5b184f2150516631be47681b9
louiscb <louiscb@kth.se>
2018-04-17 17:04:22 +0200

    report a user, sends email with user id

--- functions/db/users/report.f.js
+++ functions/db/users/report.f.js
@@ -4,3 +4,34 @@
 // function Report (UserId) do
 //   sendEmailToUs(UserID has been reported)
 // end
+const functions = require('firebase-functions');
+const admin = require('../../admin');
+const nodemailer = require('nodemailer');
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword
+    }
+});
+
+exports = module.exports = functions.https.onRequest((req, res) => {
+    let userId = req.body.userId;
+
+    const mail = {
+        from: '"Groupie-Admin." <groupiekth@gmail.com>',
+        to: 'louiscameronb@gmail.com',
+        subject: 'User: ' + userId + ' has been reported',
+        text: 'User has been reported.'
+    };
+
+    return mailTransport.sendMail(mail).then((snapshot) => {
+        return res.send('successfly reported user');
+    }).catch((error) => {
+        return res.statusCode(301).send('error: could not report user')
+    });
+});
