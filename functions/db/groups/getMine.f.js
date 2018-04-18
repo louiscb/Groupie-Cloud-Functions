191b7dabab5679d8c6e05af56a0922e66a8e5393
louiscb <louiscb@kth.se>
2018-04-18 15:15:09 +0200

    Commented useless function

--- functions/db/groups/getMine.f.js
+++ functions/db/groups/getMine.f.js
@@ -9,22 +9,22 @@
 //   }
 //   //maybe do this on the clients app?
 //   //which is faster?
-//   //we could organise the lists with other factors to do with the user
-// end
-const functions = require('firebase-functions');
-const admin = require('../../admin');
-
-exports = module.exports = functions.https.onRequest((req, res) => {
-    let userId = req.body.userId;
-    console.log(userId);
-    let path = '/users/' + userId + '/groupHistory';
-
-    return admin.database().ref(path).once('value', (snapshot) => {
-        if (snapshot.hasChildren()) {
-            let groups = snapshot.val();
-            return res.send(groups);
-        } else {
-            return res.send("null");
-        }
-    });
-});
+// //   //we could organise the lists with other factors to do with the user
+// // end
+// const functions = require('firebase-functions');
+// const admin = require('../../admin');
+//
+// exports = module.exports = functions.https.onRequest((req, res) => {
+//     let userId = req.body.userId;
+//     console.log(userId);
+//     let path = '/users/' + userId + '/groupHistory';
+//
+//     return admin.database().ref(path).once('value', (snapshot) => {
+//         if (snapshot.hasChildren()) {
+//             let groups = snapshot.val();
+//             return res.send(groups);
+//         } else {
+//             return res.send("null");
+//         }
+//     });
+// });
