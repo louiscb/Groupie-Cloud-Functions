4f1dce29b806950db3d8ec5b8d2c092d695a42cf
louiscb <louiscb@kth.se>
2018-04-20 08:40:04 +0200

    Edited subjects and topics

--- functions/db/subjects/topics/create.f.js
+++ functions/db/subjects/topics/create.f.js
@@ -9,3 +9,22 @@
 //
 //   topics.addNew(TopicName, freq = 1)
 // end
+
+const functions = require('firebase-functions');
+const admin = require('../../../admin');
+
+exports = module.exports = functions.https.onCall((data, context) => {
+    if (!context.auth)
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+
+    let subject = data.subject;
+    let topic = data.topic;
+    topic.frequency = 0;
+
+    let path = '/subjects/' + subject + '/topics/';
+
+    return admin.database().ref(path).child(topic).set(topic).then((value) => {
+        return {text: 'success'};
+        //what do we want to return?
+    });
+});
--- functions/db/subjects/topics/getSuggestions.f.js
+++
@@ -1,6 +0,0 @@
-// // send group get related active group if any
-// HTTP Request from app
-//
-// function getSuggestions (UserID) do
-//   return getTopics
-// end
